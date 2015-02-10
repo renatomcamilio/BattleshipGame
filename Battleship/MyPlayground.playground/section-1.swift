d676d1f399f9a11c9a03ff7965839d05d3497701
Renato Camilio <renatomcamilio@gmail.com>
2015-02-09 18:48:01 -0800

    Adding the `New Fleet` button to Fleet Setup screen

--- Battleship/MyPlayground.playground/section-1.swift
+++ Battleship/MyPlayground.playground/section-1.swift
@@ -1,15 +1,5 @@
 // Playground - noun: a place where people can play
 
-import UIKit
-
-
-//let boatSquares = [Square(), Square(), Square()]
-//var myBoat = Boat(size: 5, type: "AircraftCarrier", squares: boatSquares)
-//var ownFleet = Fleet(boats: [myBoat])
-//var opponentFleet = Fleet(boats: [myBoat])
-//
-//var player = Player(ownFleet: ownFleet, opponentFleet: opponentFleet)
-
-
+import Foundation
 
 
--- Boat.swift
+++ Boat.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 
+//(Aircraft carrier: 5), (Battleship: 4), (Cruiser: 3), (Destroyer: 2), (Submarine: 1)
+enum BoatSize: Int {
+    case Submarine = 1
+    case Destroyer, Cruiser, Battleship, AircraftCarrier
+}
+
 class Boat {
-    let size: Int
+    let size: BoatSize
     let type: String
+    let color: UIColor
     let squares: [Square]
     
-    init(size: Int, type: String, squares: [Square]) {
+    init(size: BoatSize, type: String, squares: [Square], color: UIColor) {
         self.size = size
         self.type = type
         self.squares = squares
+        self.color = color
     }
 }
