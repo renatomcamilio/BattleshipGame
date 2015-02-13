36984c7e79085becee0b55e7a76dd8ee3c18feae
Mike Reining <emailmike@gmail.com>
2015-02-12 21:05:58 -0800

    Added Flip animation

--- Battleship/BattleViewController.swift
+++ Battleship/BattleViewController.swift
@@ -11,11 +11,12 @@ import UIKit
 class BattleViewController: UIViewController {
     @IBOutlet weak var roundLabel: UILabel!
     @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var opponentContainerView: UIView!
     
     var battle: Battle?
     var opponentCollectionView: UICollectionView?
     var playerCollectionView: UICollectionView?
-    
+    var showingBack = true
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -48,6 +49,9 @@ class BattleViewController: UIViewController {
         }
     }
     
+    
+    
+    
     func prepareForNextTurn() {
         // TODO;"DOTO" - Mike :)
         
@@ -57,6 +61,13 @@ class BattleViewController: UIViewController {
         // My advice is to have one of that "Front UIView" in the opponent's fleet, 
         // and by default, the highlighted fleet is the activePlayer, which means that
         // it doesn't have any layer/view over it
+        
+        // Setup view for opponentContainerView animation
+//        var opponentContainerViewFront = UIView(frame: opponentContainerView.frame)
+//        opponentContainerView.addSubview(opponentContainerViewFront)
+//        opponentContainerViewFront.backgroundColor = UIColor.blackColor()
+//        opponentContainerViewFront.center = CGPointMake(150.0, 150.0)
+
         
         self.battle!.nextTurn({ isCPUPlayer in
             if isCPUPlayer {
@@ -68,10 +79,13 @@ class BattleViewController: UIViewController {
                 self.playerCollectionView?.reloadData()
             }
         })
-        
+        animateViews()
+
         self.roundLabel.text = "Round: \(String(self.battle!.round))"
         self.turnLabel.text = self.battle?.turn == 1 ? "Player turn" : "Opponent turn"
     }
+    
+    
     
     func playerSelectedCell(indexPath: NSIndexPath) {
         self.battle?.takeShot(indexPath, player: self.battle!.activePlayer!)
@@ -84,4 +98,30 @@ class BattleViewController: UIViewController {
         // maybe define the opponent as winner, because the player is giving up
         self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    func animateViews() {
+        // Setup views for animation
+        // The TRICK - add two blank views above the playerContainerView as subviews.  The animation then flips between them.
+        // Since these subviews are blank, it then animates the container view.
+        
+        // TO DO - swap out the content of the collection views.  This should be done as the players take turns along with the animation.
+        
+        // Setup view for opponentContainerView animation
+        var opponentContainerViewFront = UIView(frame: self.opponentContainerView.frame)
+        var opponentContainerViewBack = UIView(frame: self.opponentContainerView.frame)
+        self.opponentContainerView.addSubview(opponentContainerViewFront)
+        self.opponentContainerView.addSubview(opponentContainerViewBack)
+        
+        // Perform animation
+        if (showingBack) {
+            UIView.transitionFromView(opponentContainerViewBack, toView: opponentContainerViewFront, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromBottom, completion: nil)
+            showingBack = false
+        } else {
+
+            UIView.transitionFromView(opponentContainerViewFront, toView: opponentContainerViewBack, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromTop, completion: nil)
+            showingBack = true
+        }
+    }
+
+    
 }
