890c04450d392af332cda14d5c937bdf04035e94
Mike Reining <emailmike@gmail.com>
2015-02-16 14:47:23 -0800

    Cleaned up Add User View Controller and GUI

--- Battleship/AddUserViewController.swift
+++ Battleship/AddUserViewController.swift
@@ -9,70 +9,37 @@
 import UIKit
 
 class AddUserViewController: UIViewController, UINavigationControllerDelegate, UITextFieldDelegate, UIActionSheetDelegate {
-    let signupButton: UIButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
-    let loginButton: UIButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
-    let signUpLabel = UILabel(frame: CGRectMake(0, 0, 300, 30))
-    let usernameTextField = UITextField(frame: CGRectMake(20.0, 30.0, 300, 30))
-    let emailTextField = UITextField(frame: CGRectMake(20.0, 30.0, 300, 30))
-    let passwordTextField = UITextField(frame: CGRectMake(20.0, 30.0, 300, 30))
+    @IBOutlet weak var loginExistingUserButton: UIButton!
+    @IBOutlet weak var addNewUserButton: UIButton!
+    @IBOutlet weak var addPasswordTextField: UITextField!
+    
+    @IBOutlet weak var signupNewUserButton: UIButton!
+    @IBOutlet weak var addEmailTextField: UITextField!
+    @IBOutlet weak var addUsernameTextField: UITextField!
     var currentUser: PFUser?
     var userID: String?
 
+    
+    
     //MARK:
     // NOTE: I created this view programmatically so that we could avoid conflicts on the StoryBoard.  :-)
     override func viewDidLoad() {
         self.view.backgroundColor = UIColor.whiteColor()
-        
-        // Add Login Button
-        loginButton.frame = CGRectMake(20.0, 100, 300, 30)
-        loginButton.setTitle("Login", forState: UIControlState.Normal)
-        loginButton.addTarget(self, action: "loginButtonPressed", forControlEvents: UIControlEvents.TouchUpInside)
-        self.view.addSubview(loginButton)
-        
-        // Display SignUp Label
-        signUpLabel.center = CGPointMake(170, 200)
-        signUpLabel.text = "Not registered? Signup below"
-        signUpLabel.textAlignment = .Left
-        let labelFont = UIFont(name: "HelveticaNeue", size: 16)
-        signUpLabel.font = labelFont
-        view.addSubview(signUpLabel)
-        
-        // Add Username Text Field
-        usernameTextField.center = CGPointMake(170, 265)
-        usernameTextField.backgroundColor = UIColor.whiteColor()
-        usernameTextField.placeholder = " Add username"
-        usernameTextField.returnKeyType = .Next
-        self.view.addSubview(usernameTextField)
-        usernameTextField.delegate = self
-        
-        // Add Email Text Field
-        emailTextField.center = CGPointMake(170, 295)
-        emailTextField.backgroundColor = UIColor.whiteColor()
-        emailTextField.placeholder = " Add email address"
-        emailTextField.returnKeyType = .Next
-        self.view.addSubview(emailTextField)
-        emailTextField.delegate = self
-        
-        // Add Password Text Field
-        passwordTextField.center = CGPointMake(170, 325)
-        passwordTextField.backgroundColor = UIColor.whiteColor()
-        passwordTextField.placeholder = " Add password"
-        passwordTextField.returnKeyType = .Next
-        self.view.addSubview(passwordTextField)
-        passwordTextField.delegate = self
-        
-        // Add Signup Button
-        signupButton.frame = CGRectMake(20.0, 355, 300, 30)
-        signupButton.setTitle("Signup", forState: UIControlState.Normal)
-        signupButton.addTarget(self, action: "signupButtonPressed", forControlEvents: UIControlEvents.TouchUpInside)
-        self.view.addSubview(signupButton)
-        
+        addUsernameTextField.returnKeyType = .Next
+        addUsernameTextField.delegate = self
+        addEmailTextField.returnKeyType = .Next
+        addEmailTextField.delegate = self
+        addPasswordTextField.returnKeyType = .Done
+        addPasswordTextField.delegate = self
+        addUsernameTextField.hidden = true
+        addEmailTextField.hidden = true
+        addPasswordTextField.hidden = true
+        signupNewUserButton.hidden = true
         
     }
     
-    // MARK Login & SignUp
-    
-    func loginButtonPressed() {
+    @IBAction func loginExistingUserButtonPressed(sender: AnyObject) {
+        
         let alertController = UIAlertController(title: "Login to Battleship", message: "Please enter your username and password.", preferredStyle: .Alert)
         
         let loginAction = UIAlertAction(title: "Login", style: .Default) { (_) in
@@ -86,7 +53,7 @@ class AddUserViewController: UIViewController, UINavigationControllerDelegate, U
                     (user, error) in
                     // Capture user details on login
                     self.currentUser = PFUser.currentUser()
-                } 
+                }
             )
         }
         
@@ -111,20 +78,46 @@ class AddUserViewController: UIViewController, UINavigationControllerDelegate, U
         self.presentViewController(alertController, animated: true) {
             // ...
         }
-
+        
     }
     
+    @IBAction func addNewUserButtonPressed(sender: AnyObject) {
+        addUsernameTextField.hidden = false
+        addEmailTextField.hidden = false
+        addPasswordTextField.hidden = false
+        signupNewUserButton.hidden = false
+        addUsernameTextField.becomeFirstResponder()
+
+    }
+
 
     
-
-    
-    func signupButtonPressed() {
-        var userEntered = usernameTextField.text
-        var emaillEntered = emailTextField.text
-        var passwordEntered = passwordTextField.text
-        if userEntered != "" && emaillEntered != "" && passwordEntered != "" {
+    func userSignUp(username: String, email: String, password: String) {
+        if username != "" && email != "" && password != "" {
             // If signup looks valid, save login in Parse
-            userSignUp(userEntered, email: emaillEntered, password: passwordEntered)
+            var user = PFUser()
+            user.username = username
+            user.email = email
+            user.password = password
+            user.signUpInBackgroundWithBlock { (success, error) -> Void in
+                if success {
+                    self.currentUser = PFUser.currentUser()
+                    self.userID = PFUser.currentUser().objectId!
+                    println(self.currentUser)
+                    println(self.userID)
+                    let alert = UIAlertController(title: "Success", message: "Your user account was added successfully", preferredStyle: .Alert)
+                    let action = UIAlertAction(title: "Sweet!", style: .Default) { (action: UIAlertAction!) -> Void in
+                    }
+                    alert.addAction(action)
+                    self.presentViewController(alert, animated: true, completion: nil)
+                } else {
+                    let alert = UIAlertController(title: "Something went wrong", message: "Please try again", preferredStyle: .Alert)
+                    let action = UIAlertAction(title: "Ok, got it!", style: .Default) { (action: UIAlertAction!) -> Void in
+                    }
+                    alert.addAction(action)
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+            }
         } else {
             // If signup is invalid show alert view
             var alert = UIAlertController(title: "Missing Details", message: "Please check the form and try again.", preferredStyle: .Alert)
@@ -136,31 +129,39 @@ class AddUserViewController: UIViewController, UINavigationControllerDelegate, U
             
             presentViewController(alert, animated: true, completion: nil)
         }
+
+        
+        
+        
     }
     
-    func userSignUp(username: String, email: String, password: String) {
-        var user = PFUser()
-        user.username = username
-        user.email = email
-        user.password = password
-        user.signUpInBackgroundWithBlock { (success, error) -> Void in
-            if success {
-                self.currentUser = PFUser.currentUser()
-                self.userID = PFUser.currentUser().objectId!
-                println(self.currentUser)
-                println(self.userID)
-                let alert = UIAlertController(title: "Success", message: "Your user account was added successfully", preferredStyle: .Alert)
-                let action = UIAlertAction(title: "Sweet!", style: .Default) { (action: UIAlertAction!) -> Void in
-                }
-                alert.addAction(action)
-                self.presentViewController(alert, animated: true, completion: nil)
-            } else {
-                let alert = UIAlertController(title: "Something went wrong", message: "Please try again", preferredStyle: .Alert)
-                let action = UIAlertAction(title: "Ok, got it!", style: .Default) { (action: UIAlertAction!) -> Void in
-                }
-                alert.addAction(action)
-                self.presentViewController(alert, animated: true, completion: nil)
-            }
-        }
+    @IBAction func signupNewUserButtonPressed(sender: AnyObject) {
+        var username = addUsernameTextField.text
+        var email = addEmailTextField.text
+        var password = addPasswordTextField.text
+        userSignUp(username, email: email, password: password)
+        
     }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {   //delegate method
+        if textField == addUsernameTextField {
+            addUsernameTextField.resignFirstResponder()
+            addEmailTextField.becomeFirstResponder()
+            return true
+        }
+        
+        if textField == addEmailTextField {
+            addEmailTextField.resignFirstResponder()
+            addPasswordTextField.becomeFirstResponder()
+            return true
+        }
+        
+        if textField == addPasswordTextField {
+            addPasswordTextField.resignFirstResponder()
+            userSignUp(addUsernameTextField.text, email: addEmailTextField.text, password: addPasswordTextField.text)
+        }
+        
+        return false
+    }
+
 }
